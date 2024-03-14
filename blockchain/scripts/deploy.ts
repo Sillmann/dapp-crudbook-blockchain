a0de710066af2eb6de7a6a36e5c1be0ce6a1b26d
ssilman <ssilman@gmail.com>
2024-03-13 21:46:54 -0300

    atualizacao

---
+++ blockchain/scripts/deploy.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+
+  const bookdatabase = await ethers.deployContract("BookDatabase");
+
+  await bookdatabase.waitForDeployment();
+
+  const address = await bookdatabase.getAddress();
+
+  console.log(`Deploy finished at ${address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
